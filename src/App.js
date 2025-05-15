1d848106ca41a5c8ecd2730b8d1b0a19c5749d13
nikhilsk <nikhils2400@gmail.com>
2025-05-15 23:13:48 +0530

    js vs react example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+
+  const [counter, setCounter] = useState(0)
   return (
     <div>
       <h2>Let's get started!</h2>
+
+    <div>
+      Counter value in react: {counter}
+    </div>
+    <button onClick={()=>setCounter(prev=>prev+1)}>CLICK ME</button>
     </div>
   );
 }

6ef1ca44b8a33a4669c2967a35ffa46f0369dce2
nikhilsk <nikhils2400@gmail.com>
2025-05-16 23:53:07 +0530

    2 pages working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import MenuPage from "./components/MenuPage";
 
 function App() {
     return (
-        <div id="root">
-            <LandingPage />
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route path="/menu" component={MenuPage} />
+            </Switch>
+        </Router>
     );
 }
 

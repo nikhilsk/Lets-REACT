8fe7220bda018f6c2af5b6a28c3270b2562f3abc
nikhilsk <nikhils2400@gmail.com>
2025-05-17 00:39:36 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/LandingPage";
 import MenuPage from "./components/MenuPage";
-import Show from "./components/Show";
+// import Show from "./components/Show";
 
 function App() {
     return (
         <Router>
             <Switch>
-                {/* <Route exact path="/" component={LandingPage} /> */}
-                <Route exact path="/" component={Show} />
+                <Route exact path="/" component={LandingPage} />
+                {/* <Route exact path="/" component={Show} /> */}
                 <Route path="/menu" component={MenuPage} />
                 {/* YOUR TASK  */}
                 {/* <Route path="/order" component={OrderPage} />

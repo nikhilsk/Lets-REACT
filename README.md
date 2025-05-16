0cb9c8e29cd50e83c2ff322293e824b880f79ad6
nikhilsk <nikhils2400@gmail.com>
2025-05-16 23:15:01 +0530

    restaurant app

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Workshop 🚀
+
+This is a hands-on session to explore React fundamentals.  
+Learn to create reusable components and build interactive UIs!
+
+## Setup
+1. Clone the repository.
+2. Run `npm install` to install dependencies.
+3. Start the project with `npm start`.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { useState } from "react";
+import React from "react";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-
-  const [counter, setCounter] = useState(0)
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-
-    <div>
-      Counter value in react: {counter}
-    </div>
-    <button onClick={()=>setCounter(prev=>prev+1)}>CLICK ME</button>
-    </div>
-  );
+    return (
+        <div id="root">
+            <LandingPage />
+        </div>
+    );
 }
 
 export default App;
